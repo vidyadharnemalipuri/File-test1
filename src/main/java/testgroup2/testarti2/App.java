ad438c856ec59c94291a645d300f29e369690835
Indu Katta <vindukatta@gmail.com>
2018-09-05 23:53:15 +0100

    initial commit

---
+++ src/main/java/testgroup2/testarti2/App.java
@@ -0,0 +1,13 @@
+package testgroup2.testarti2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
